a712418184a0450bf52353f0431b29f62c4d8e06
Jonathan Dunlap <jonnyd2885@gmail.com>
2022-12-09 14:25:06 -0800

    Update variables to match more closely to DB

--- src/main/java/com/revature/models/LoginDTO.java
+++ src/main/java/com/revature/models/LoginDTO.java
@@ -1,13 +1,13 @@
 package com.revature.models;
 
 public class LoginDTO {
-	public String email;
+	public String username;
 	public String password;
 	public boolean isManager;
 
-	public LoginDTO(String email, String password, boolean isManager) {
+	public LoginDTO(String username, String password, boolean isManager) {
 		super();
-		this.email = email;
+		this.username = username;
 		this.password = password;
 		this.isManager = isManager;
 	}
