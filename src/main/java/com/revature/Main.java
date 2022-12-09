45670b429ce1d172fec37f3180543f7b30b5c369
Jonathan Dunlap <jonnyd2885@gmail.com>
2022-12-09 14:41:04 -0800

    Create javalin server, add logincontroller

--- src/main/java/com/revature/Main.java
+++ src/main/java/com/revature/Main.java
@@ -1,5 +1,24 @@
 package com.revature;
 
-public class Main {
+import com.revature.controllers.Controller;
+import com.revature.controllers.LoginController;
 
+import io.javalin.Javalin;
+
+public class Main {
+	private static Javalin app;
+
+	public static void main(String[] args) {
+		app = Javalin.create();
+
+		configure(new LoginController());
+
+		app.start(8083);
+	}
+
+	public static void configure(Controller... controllers) {
+		for (Controller controller : controllers) {
+			controller.addRoutes(app);
+		}
+	}
 }
