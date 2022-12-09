5d92a2244c8d06516a9ef102119613b8d4f910e9
Jonathan Dunlap <jonnyd2885@gmail.com>
2022-12-09 13:57:01 -0800

    Create userDAOImpl, generate methods

---
+++ src/main/java/com/revature/daos/UserDAOImpl.java
@@ -0,0 +1,27 @@
+package com.revature.daos;
+
+import java.util.List;
+
+import com.revature.models.User;
+
+public class UserDAOImpl implements UserDAO {
+
+	@Override
+	public boolean addUser(User user) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public User getUser(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<User> getUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
