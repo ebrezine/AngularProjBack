fcb5c1fd2cb253c741d3f8a0a815329c024cc06d
Jonathan Dunlap <jonnyd2885@gmail.com>
2022-12-09 13:49:04 -0800

    Fix issues in SQL statment, save in project folder

---
+++ project2.sql
@@ -0,0 +1,28 @@
+CREATE TABLE users (
+id SERIAL PRIMARY KEY,
+username VARCHAR(500) UNIQUE,
+PASSWORD VARCHAR(500),
+is_worker BOOLEAN
+);
+
+CREATE TABLE claims (
+id SERIAL,
+amount INTEGER,
+description VARCHAR(500),
+status VARCHAR(8),
+created_by INTEGER references users(id),
+pending BOOLEAN
+);
+
+CREATE TABLE covid (
+id INTEGER references users(id),
+has_covid BOOLEAN,
+had_covid BOOLEAN,
+date_updated DATE
+);
+
+
+-- FOR TESTING/DEBUGGING
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS claims;
+DROP TABLE IF EXISTS covid;
